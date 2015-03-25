7b48507cd8ac7933609470bf024d6a643140d203
Zubair Hamed <zubairhamed@gmail.com>
2015-03-25 08:16:25 +0800

    Unstable commit

--- client.go
+++ client.go
@@ -29,6 +29,7 @@ func (c *Client) Bootstrap(ep string, fn goap.MessageHandler) {
     }
 }
 
+// TODO: Return EndPoint URL
 func (c *Client) Register(ep string, fn goap.MessageHandler) {
     msg := goap.NewMessageOfType(goap.TYPE_CONFIRMABLE, 12345)
     msg.Code = goap.POST
@@ -40,4 +41,82 @@ func (c *Client) Register(ep string, fn goap.MessageHandler) {
     if err != nil {
         log.Println (err)
     }
+
+    // /rd?ep={Endpoint Client Name}&lt={Lifetime}&sms={MSISDN} &lwm2m={version}&b={binding}
 }
+
+func (c *Client) Deregister(ins string, fn goap.MessageHandler) {
+    msg := goap.NewMessageOfType(goap.TYPE_CONFIRMABLE, 12345)
+    msg.Code = goap.DELETE
+    msg.AddOptions(goap.NewPathOptions(ins))
+    err := c.coapClient.SendAsync(msg, fn)
+    if err != nil {
+        log.Println (err)
+    }
+}
+
+/*
+    Update
+    PUT
+    /{location}?lt={Lifetime}&sms={MSISDN} &b={binding}
+
+    ---
+    Deregister
+    DELETE
+    /{location}
+
+    ---
+    Read
+    GET
+    /{Object ID}/{Object Instance ID}/{Resource ID}
+
+    ---
+    Discover
+    GET Accept: application/link- format
+    /{Object ID}/{Object Instance ID}/{Resource ID}
+
+    ---
+    Write
+    PUT/POST
+    /{Object ID}/{Object Instance ID}/{Resource ID}
+
+    ---
+    Write Attributes
+    PUT
+    /{Object ID}/{Object Instance ID}/{Resource ID}?pmin={minimum period}&pmax={maximum period}&gt={greater than}&lt={less than}&st={step}&cancel
+
+    ---
+    Execute
+    POST
+    /{Object ID}/{Object Instance ID}/{Resource ID}
+
+    ---
+    Create
+    POST
+    /{Object ID}/{Object Instance ID}
+
+    ---
+    Delete
+    DELETE
+    /{Object ID}/{Object Instance ID}
+
+    ---
+    Observe
+    GET with Observe Option
+    /{Object ID}/{Object Instance ID}/{Resource ID}
+
+    ---
+    Cancel Observation
+    Reset message
+
+    ---
+    Notify
+    Asynchronous Response
+
+    ---
+
+
+
+
+*/
+
--- examples/client/basic.go
+++ examples/client/basic.go
@@ -4,6 +4,7 @@ import (
 	"github.com/zubairhamed/golwm2m"
     "github.com/zubairhamed/goap"
     "log"
+    "time"
 )
 
 func main() {
@@ -11,8 +12,16 @@ func main() {
 
 	client.Dial("udp", "localhost", 5683)
 
-    client.Register("golwm2m", func (msg *goap.Message){
+    client.Register("golwm2ma", func (msg *goap.Message){
         log.Println(goap.CoapCodeToString(msg.Code))
+        log.Println(msg.GetOption(goap.OPTION_LOCATION_PATH))
+
+        log.Println(msg.GetPath())
+
+        time.Sleep(5 * time.Second)
+
+        log.Println("Deregistering..")
+        // client.Deregister()
     })
 }
 
