316ea66bd8db3acee3eb6fe2399da548794f2e61
Zubair Hamed <zubair.mohammad@bosch-si.com>
2015-04-28 18:07:11 +0800

    no message

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
     . "github.com/zubairhamed/goap"
     "net"
     "log"
+    "errors"
 )
 
 func NewLWM2MClient(local string, remote string) (*LWM2MClient) {
@@ -17,7 +18,7 @@ func NewLWM2MClient(local string, remote string) (*LWM2MClient) {
 
     return &LWM2MClient{
         coapServer: coapServer,
-        instances: []*ObjectInstance{},
+        enabledObjects: make(LWM2MObjectInstances),
     }
 }
 
@@ -29,10 +30,13 @@ type FnOnRegistered func(string)
 type FnOnUnregistered func()
 type FnOnError func()
 
+type LWM2MObjectInstances map[LWM2MObjectType][]*ObjectInstance
+
 type LWM2MClient struct {
     coapServer          *CoapServer
     registry            *ObjectRegistry
-    instances           []*ObjectInstance
+    // instances           []*ObjectInstance
+    enabledObjects      LWM2MObjectInstances
 
     // Events
     evtOnStartup        FnOnStartup
@@ -48,17 +52,18 @@ type LWM2MClient struct {
 func (c *LWM2MClient) Start() {
     s := c.coapServer
     s.OnStartup(func(evt *Event) {
-        // CallEvent(c.evtOnStartup, EmptyEventPayload())
+        if c.evtOnStartup != nil {
+            c.evtOnStartup()
+        }
     })
-
     c.coapServer.Start()
 }
 
 func (c *LWM2MClient) Register(name string) (string) {
     req := NewRequest(TYPE_CONFIRMABLE, POST, GenerateMessageId())
 
-    req.SetStringPayload(BuildModelResourceStringPayload(c.instances))
-    log.Println(BuildModelResourceStringPayload(c.instances))
+    req.SetStringPayload(BuildModelResourceStringPayload(c.enabledObjects))
+    log.Println(BuildModelResourceStringPayload(c.enabledObjects))
     req.SetRequestURI("rd")
     req.SetUriQuery("ep", name)
     resp, err := c.coapServer.Send(req)
@@ -98,25 +103,49 @@ func (c *LWM2MClient) UseRegistry(reg *ObjectRegistry) {
 }
 
 func (c *LWM2MClient) EnableObject(t LWM2MObjectType) {
-    c.instances = append(c.instances, NewObjectInstance(t))
+    if c.enabledObjects[t] != nil {
+        c.enabledObjects[t] = append(c.enabledObjects[t], NewObjectInstance(t))
+    }
+}
+
+func (c *LWM2MClient) AddObjectInstance(instance *ObjectInstance) (error) {
+    if instance != nil {
+        o := c.GetObjectInstance(instance.TypeId, instance.Id)
+        if o == nil {
+            c.enabledObjects[instance.TypeId] = append(c.enabledObjects[instance.TypeId], o)
+
+            return nil
+        } else {
+            return errors.New("Instance already exists. Use UpdateObjectInstance instead")
+        }
+    } else {
+        return errors.New("Attempting to add a nil instance")
+    }
+
 }
 
 func (c *LWM2MClient) AddObjectInstances (instances ... *ObjectInstance) {
     for _, o := range instances {
-        inst := c.GetObjectInstance(o.TypeId, o.Id)
-
-        if inst == nil {
-            c.instances = append(c.instances, o)
-        } else {
-            // TODO: Throw Error
-        }
+        log.Println("Add Object Instance")
+        log.Println(o)
+        c.AddObjectInstance(o)
     }
 }
 
-func (c *LWM2MClient) GetObjectInstance(n LWM2MObjectType, inst int) (*ObjectInstance) {
-    for _, o := range c.instances {
-        if o.Id == inst && o.TypeId == n {
-            return o
+func (c *LWM2MClient) GetObjectInstance(n LWM2MObjectType, instance int) (*ObjectInstance) {
+    obj := c.enabledObjects[n]
+    log.Println("!!!!")
+    log.Println(c.enabledObjects)
+
+    if obj != nil {
+        log.Println(len(obj))
+        log.Println(obj)
+        if len(obj) > 0 {
+            for _, o := range obj {
+                if o.Id == instance && o.TypeId == n {
+                    return o
+                }
+            }
         }
     }
     return nil
@@ -152,6 +181,41 @@ func (c *LWM2MClient) OnError (fn FnOnError) {
 }
 
 // Functions
-func BuildModelResourceStringPayload(inst []*ObjectInstance) string {
+func BuildModelResourceStringPayload(instances LWM2MObjectInstances) (string) {
+    // var buf bytes.Buffer
+
+    for k := range instances {
+        log.Println(instances[k])
+    }
+    /*
+    for _, instance := range instances {
+        typeId := instance.TypeId
+        if len(instance.Resources) > 0 {
+            for _, res := range instance.Resources {
+                buf.WriteString(fmt.Sprintf("</%d/%d>,", typeId, i))
+            }
+        }
+    }
+    */
+
     return ""
 }
+
+
+/*
+    var buf bytes.Buffer
+
+    for _, r := range resources {
+        log.Println (r.model)
+        resourceId := r.model.Id
+        if len(r.instances) > 0 {
+            for _, i := range r.instances {
+                buf.WriteString(fmt.Sprintf("</%d/%d>,", resourceId, i))
+            }
+        } else {
+            buf.WriteString(fmt.Sprintf("</%d>,", resourceId))
+        }
+    }
+
+    return buf.String()
+*/
