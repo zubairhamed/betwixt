4ec91db00023ec0240820c1b3289f9602bb3787c
Zubair Hamed <zubair.mohammad@bosch-si.com>
2015-06-03 17:43:41 +0800

    UI Changes to server home

--- server/httprequests.go
+++ server/httprequests.go
@@ -3,13 +3,15 @@ package server
 import (
 	"github.com/zubairhamed/betwixt/server/pages"
 	. "github.com/zubairhamed/go-commons/network"
+	"log"
 )
 
 func SetupHttpRoutes(server *DefaultServer) {
 	http := server.httpServer
 
 	http.NewRoute("/", METHOD_GET, handleHttpHome(server))
-	http.NewRoute("/client/{client}", METHOD_GET, handleHttpViewClient(server))
+	http.NewRoute("/client/{client}/view", METHOD_GET, handleHttpViewClient(server))
+	http.NewRoute("/client/{client}/delete", METHOD_GET, handleHttpDeleteClient(server))
 }
 
 func handleHttpViewClient(server *DefaultServer) RouteHandler {
@@ -28,6 +30,18 @@ func handleHttpViewClient(server *DefaultServer) RouteHandler {
 	}
 }
 
+func handleHttpDeleteClient(server *DefaultServer) RouteHandler {
+	return func(r Request) Response {
+		page := &pages.HomePage{}
+
+		log.Println("Handle Deleting of Client")
+
+		return &HttpResponse{
+			Payload:       NewBytesPayload(page.GetContent()),
+		}
+	}
+}
+
 func handleHttpHome(server *DefaultServer) RouteHandler {
 	return func(r Request) Response {
 
@@ -40,22 +54,29 @@ func handleHttpHome(server *DefaultServer) RouteHandler {
 			LastUpdate       string
 		}
 
-		model := []client{}
+		type model struct {
+			Clients 		 []*client
+			ClientsCount 	 int
+		}
 
+		cl := []*client{}
 		for _, v := range server.clients {
-			c := client{
+			c := &client{
 				Endpoint:         v.GetName(),
 				RegistrationID:   v.GetId(),
 				RegistrationDate: v.GetRegistrationDate().Format("Jan 2, 2006, 3:04pm (SGT)"),
 				LastUpdate:       v.LastUpdate().Format("Jan 2, 2006, 3:04pm (SGT)"),
 			}
+			cl = append(cl, c)
+		}
 
-
-			model = append(model, c)
+		m := &model{
+			ClientsCount: len(cl),
+			Clients: cl,
 		}
 
 		return &HttpResponse{
-			TemplateModel: model,
+			TemplateModel: m,
 			Payload:       NewBytesPayload(page.GetContent()),
 		}
 	}
--- server/pages/HomePage.go
+++ server/pages/HomePage.go
@@ -11,7 +11,7 @@ func (p *HomePage) content() string {
 	return `
         <html>
             <head>
-                <title>Registered Clients</title>
+                <title>Betwixt</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -32,10 +32,60 @@ func (p *HomePage) content() string {
                 </nav>
 
                 <div class="container theme-showcase" role="main">
-                  <br /><br />
+                    <br /><br /><br />
+
+                <div class="row" style="text-align: center">
+                    <div class="col-sm-2">&nbsp;</div>
+
+                    <div class="col-sm-2">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Memory (MB)</h3>
+                            </div>
+                            <div class="panel-body">
+                              <h1>10.2</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Clients</h3>
+                            </div>
+                            <div class="panel-body">
+                              <h1>{{.ClientsCount}}</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="panel panel-success">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Requests</h3>
+                            </div>
+                            <div class="panel-body">
+                              <h1>200212</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="panel panel-danger">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Errors</h3>
+                            </div>
+                            <div class="panel-body">
+                              <h1>3</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">&nbsp;</div>
+                  </div>
 
                   <div class="page-header">
-                    <h1>Registered Clients</h1>
+                    <h3>Registered Clients</h3>
                   </div>
                   <div class="row">
 
@@ -52,16 +102,16 @@ func (p *HomePage) content() string {
                         </thead>
                         <tbody>
 
-                          {{ range . }}
+                          {{ range .Clients }}
                           <tr>
-                            <td>{{.Endpoint}}</td>
+                            <td><a href="/client/{{.RegistrationID}}/view">{{.Endpoint}}</a></td>
                             <td>{{.RegistrationID}}</td>
                             <td>{{.RegistrationDate}}</td>
                             <td>{{.LastUpdate}}</td>
                             <td>
                               <h4>
-                                <span class="label label-info">View</span>
-                                <span class="label label-danger">Remove</span>
+                                <span class="label label-info"><a href="/client/{{.RegistrationID}}/view">View</a></span>
+                                <span class="label label-danger"><a href="/client/{{.RegistrationID}}/delete">Remove</a></span>
                               </h4>
                             </td>
                           </tr>
