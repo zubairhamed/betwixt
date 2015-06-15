df68c956a8a3a17e96f80eda5959ecb1558b1a06
Zubair Hamed <zubairhamed@gmail.com>
2015-06-16 00:05:20 +0800

    Initial code for Validators

---
+++ core/values/validators/validators.go
@@ -0,0 +1,37 @@
+package validators
+
+type Validator interface {
+	Valid(val interface{}) bool
+}
+
+func NewRangeValidator(from int64, to int64) Validator {
+	return &RangeValidator{
+		from: from,
+		to: to,
+	}
+}
+
+type RangeValidator struct {
+	from 	int64
+	to 		int64
+}
+
+func (v *RangeValidator) Valid(val interface{}) bool {
+	return true
+}
+
+func NewLengthValidator(len uint64) Validator {
+	return &LengthValidator {
+		len: len,
+	}
+}
+
+type LengthValidator struct {
+	len 	uint64
+}
+
+func (v *LengthValidator) Valid(val interface{}) bool {
+	return true
+}
+
+
